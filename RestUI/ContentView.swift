8ab20ebb652fa86bd52511f749bf52a006a04e4d
Maxim Vainikka <rithmique@yahoo.com>
2021-04-21 15:31:08 +1000

    Initial Commit

---
+++ RestUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestUI
+//
+//  Created by Максим Нуждин on 21.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestUI/RestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestUIApp.swift
+//  RestUI
+//
+//  Created by Максим Нуждин on 21.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct RestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
